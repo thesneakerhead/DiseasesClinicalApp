b67898e403f722e103752f26b061c55f310bdb8d
thesneakerhead <69101485+thesneakerhead@users.noreply.github.com>
2021-08-29 00:21:58 +0800

    login activities

--- DiseasesClinicalApp/app/src/main/java/com/cz3002/diseasesclinicalapp/FirebaseLogin.java
+++ DiseasesClinicalApp/app/src/main/java/com/cz3002/diseasesclinicalapp/FirebaseLogin.java
@@ -1,20 +1,35 @@
 package com.cz3002.diseasesclinicalapp;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.firebase.ui.auth.util.ExtraConstants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class FirebaseLogin extends AppCompatActivity {
 
@@ -22,7 +37,17 @@ public class FirebaseLogin extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
     private List<AuthUI.IdpConfig> providers;
-
+    private Button signoutbutton;
+    private TextView idText;
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
     @Override
     protected void onStart() {
         super.onStart();
@@ -39,17 +64,40 @@ public class FirebaseLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         firebaseAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.firebaselogin);
         init();
+        signoutbutton = (Button)findViewById(R.id.signout_button);
+        idText = (TextView) findViewById(R.id.idText) ;
+        signoutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(FirebaseLogin.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // ...
+                                createSignInPage();
+                            }
+                        });
+            }
+        });
+        if (AuthUI.canHandleIntent(getIntent())) {
+            if (getIntent().getExtras() == null) {
+                return;
+            }
+            String link = getIntent().getExtras().getString(ExtraConstants.EMAIL_LINK_SIGN_IN);
+            if (link != null) {
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setEmailLink(link)
+                        .setAvailableProviders(providers)
+                        .build();
+                signInLauncher.launch(signInIntent);
+            }
+        }
     }
-    private void init() {
-        providers = Arrays.asList(
-                new AuthUI.IdpConfig.GoogleBuilder().build(), // Log in using Google
-                new AuthUI.IdpConfig.EmailBuilder().build(), // Log in using Email
-                new AuthUI.IdpConfig.FacebookBuilder().build(), // Log in using Facebook
-                new AuthUI.IdpConfig.PhoneBuilder().build() //Log in using Phone
-        );
 
+    private void init() {
         listener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -59,23 +107,47 @@ public class FirebaseLogin extends AppCompatActivity {
 
                     Toast.makeText(FirebaseLogin.this, "There is an account logged in: " + user.getUid(), Toast.LENGTH_SHORT).show();
                 } else {
-                    startActivityForResult(AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(), AUTH_REQUEST_CODE);
+                    createSignInPage();
                 }}
         };
         firebaseAuth.addAuthStateListener(listener);
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        CallbackManager mCallbackManager = CallbackManager.Factory.create();
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        CallbackManager mCallbackManager = CallbackManager.Factory.create();
+//        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        // Pass the activity result back to the Facebook SDK
+//
+//    }
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            idText.setText(user.getUid());
+            // ...
+        } else {
 
-        // Pass the activity result back to the Facebook SDK
+        }
+    }
+    public void createSignInPage()
+    {
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(), // Log in using Google
+                new AuthUI.IdpConfig.EmailBuilder().build(), // Log in using Email
+                new AuthUI.IdpConfig.FacebookBuilder().build(), // Log in using Facebook
+                new AuthUI.IdpConfig.PhoneBuilder().build() //Log in using Phone
+        );
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
 
     }
 }
